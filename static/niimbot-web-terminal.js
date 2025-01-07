6fa7476f91b7e029206c6d851b672112ca7b76c5
MultiMote <contact@mmote.ru>
2025-01-07 12:46:12 +0300

    Add macros

--- static/niimbot-web-terminal.js
+++ static/niimbot-web-terminal.js
@@ -8,6 +8,7 @@
     const sendHexButton = document.querySelector('.send-hex');
     const sendConstructedButton = document.querySelector('.send-construct');
     const hexInput = document.querySelector('.hex-command');
+    const macrosContainer = document.querySelector('.macros');
 
     const constructElements = {
         size: document.querySelector('.construct-size'),
@@ -19,19 +20,57 @@
     let gattServer = null;
     let channel = null;
 
+    const macroBtn = macrosContainer.querySelector("button");
+    const macroItem1 = document.querySelector(".dropdown-menu.save-raw-macro .macro")
+    const macroItem2 = document.querySelector(".dropdown-menu.save-construct-macro .macro")
+
+    for (let i = 1; i <= 24; i++) {
+        const btn = macroBtn.cloneNode();
+        btn.textContent = `M${i}`;
+        btn.onclick = async () => {
+            const value = localStorage.getItem(btn.textContent) ?? "";
+            const bytes = hexToBuf(value);
+            await sendBytes(bytes);
+        }
+        macroBtn.parentElement.appendChild(btn);
+
+        const li1 = macroItem1.cloneNode(true);
+        const a1 = li1.querySelector("a");
+        a1.textContent = `M${i}`;
+        a1.onclick = (e) => {
+            e.preventDefault();
+            localStorage.setItem(a1.textContent, hexInput.value);
+        }
+        macroItem1.parentElement.appendChild(li1);
+
+        const li2 = macroItem2.cloneNode(true);
+        const a2 = li2.querySelector("a");
+        a2.textContent = `M${i}`;
+        a2.onclick = (e) => {
+            e.preventDefault();
+            localStorage.setItem(a1.textContent, bufToHex(getConstructed()));
+        }
+        macroItem2.parentElement.appendChild(li2);
+    }
+
+    macroBtn.remove();
+    macroItem1.remove();
+    macroItem2.remove();
+
+
     const updateControls = (connected) => {
         if (connected) {
             connectButton.setAttribute("disabled", "");
             disconnectButton.removeAttribute("disabled");
-            hexInput.removeAttribute("disabled");
             sendHexButton.removeAttribute("disabled");
             sendConstructedButton.removeAttribute("disabled");
+            macrosContainer.querySelectorAll("button").forEach(e => e.removeAttribute("disabled"));
         } else {
             disconnectButton.setAttribute("disabled", "");
             connectButton.removeAttribute("disabled");
-            hexInput.setAttribute("disabled", "");
             sendHexButton.setAttribute("disabled", "");
             sendConstructedButton.setAttribute("disabled", "");
+            macrosContainer.querySelectorAll("button").forEach(e => e.setAttribute("disabled", ""));
         }
     }
 
@@ -51,8 +90,6 @@
         } else {
             arr = buf;
         }
-
-        console.log(arr)
 
         return arr.map(
             (i) => i.toString(16).padStart(2, "0").toUpperCase()
@@ -164,28 +201,28 @@
         disconnect();
     };
 
-    const sendHex = async () => {
+    const sendBytes = async (bytes) => {
         if (channel === null) {
             return;
         }
 
-        const bytes = hexToBuf(hexInput.value);
-
-        if (!bytes.length == 0) {
+        if (bytes.length === 0) {
             return;
         }
 
         logger(`>> ${bufToHex(bytes)}`);
-        await channel.writeValueWithoutResponse(bytes);
+
+        await channel.writeValueWithoutResponse(new Uint8Array(bytes));
     };
 
-    const sendConstructed = async () => {
-        if (channel === null) {
-            return;
-        }
+    const sendHex = async () => {
+        const bytes = hexToBuf(hexInput.value);
+        await sendBytes(bytes);
+    };
 
+    const getConstructed = () => {
         const bytes = [];
-        
+
         bytes.push(0x55, 0x55);
         bytes.push(...hexToBuf(constructElements.command.value));
         bytes.push(...hexToBuf(constructElements.size.value));
@@ -193,9 +230,8 @@
         bytes.push(...hexToBuf(constructElements.checksum.value));
         bytes.push(0xaa, 0xaa);
 
-        logger(`>> ${bufToHex(bytes)}`);
-        await channel.writeValueWithoutResponse(new Uint8Array(bytes));
-    };
+        return bytes;
+    }
 
     hexInput.addEventListener("keypress", async (e) => {
         if (e.key === "Enter") {
@@ -209,7 +245,7 @@
     };
 
     sendConstructedButton.onclick = async () => {
-        await sendConstructed();
+        await sendBytes(getConstructed());
     };
 
     constructElements.data.oninput = () => {
